b81e7b30761b0a783529b252cd854b5808c49732
MissLuckyBubble <qvkatadlg9@gmail.com>
2023-11-05 10:15:58 +0200

    CRUD to Category

--- src/main/java/uni/fmi/Solaris/services/ICategoryService.java
+++ src/main/java/uni/fmi/Solaris/services/ICategoryService.java
@@ -6,4 +6,15 @@ import java.util.List;
 
 public interface ICategoryService {
     List<CategoryDTO> getAll();
+    /**
+    * Find Category by id
+     * @param id
+     * @return CategoryDTO
+     *
+    **/
+    CategoryDTO getBy(Long id);
+
+    CategoryDTO create(CategoryDTO categoryDTO);
+    CategoryDTO update(CategoryDTO categoryDTO);
+    boolean delete(long id);
 }
